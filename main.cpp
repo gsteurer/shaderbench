3fad44bd5ebfbf45d570a63868e7a1c7209c0413
Joseph A. (Gus) Steurer <joseph.a.steurer@outlook.com>
2021-12-23 00:08:31 -0600

    fix title

--- main.cpp
+++ main.cpp
@@ -147,7 +147,7 @@ Window::Window() {
     // create instance and surface
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Hello Triangle";
+    appInfo.pApplicationName = "Shaderbench";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
